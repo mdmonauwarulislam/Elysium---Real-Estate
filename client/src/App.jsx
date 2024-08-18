d1933ecd6a648eb7f48ff7c93adec46e00200bce
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-08-19 01:08:49 +0530

    Setup the project

---
+++ client/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <div className="bg-zinc-800">
+      <h1 className="text- text-black xl text-center pt-10">Hello World</h1>
+    </div>
+  )
+}
+
+export default App
