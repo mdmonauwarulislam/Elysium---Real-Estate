d3cce79274533e0689e04417f51cde9fa653ce01
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-08-20 00:59:42 +0530

    create user models

--- api/index.js
+++ api/index.js
@@ -1,11 +1,54 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routes/user.route.js";
+import authRouter from "./routes/auth.route.js";
+import listingRouter from "./routes/listing.route.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+dotenv.config();
+
+//I need apllication string
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = express();
+//allowing json form as input
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-    });
+app.use(cookieParser());
 
-app.listen(5000, () => {
-    console.log('Server is running on http://localhost:5000');
-    });
+const corsOptions = {
+  origin: true,
+};
+app.use(cors(corsOptions));
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000!!`);
+});
+
+//api route
+//req is from client
+//res is from server
+
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
+
+//middleware-I use it in auth.controller
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  const message = err.message || "Internal Server Error";
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
