d8398b79d39c57afede73da7e602982775187c1c
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-08-27 23:26:40 +0530

    add listing functionality

--- api/utils/error.js
+++ api/utils/error.js
@@ -1,7 +1,11 @@
+//it will use in auth.controller
+//I need this custom error function to handle
+//situation like putting less characters for password
+//its not an error but need to handle
 export const errorHandler = (statusCode, message) => {
-    const error = new Error();
-    error.statusCode = statusCode;
-    error.messgae = message;
-  
-    return error;
-  };
+  const error = new Error();
+  error.statusCode = statusCode;
+  error.messgae = message;
+
+  return error;
+};
--- api/utils/verifyUser.js
+++ api/utils/verifyUser.js
@@ -1,19 +1,19 @@
 import { errorHandler } from "./error.js";
 import jwt from "jsonwebtoken";
 export const verifyToken = (req, res, next) => {
-    //I am checking token in cookies-i need cookie parser
-    //install cookieparser and import in index.js
+  //I am checking token in cookies-i need cookie parser
+  //install cookieparser and import in index.js
 
-    const token = req.cookies.access_token;
+  const token = req.cookies.access_token;
 
-    if (!token) {
-        return next(errorHandler(401, " Unauthorized"));
-    }
+  if (!token) {
+    return next(errorHandler(401, " Unauthorized"));
+  }
 
-    jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
-        if (err) return next(errorHandler(403, "Not allowed"));
-        //user is from cookie-id send it to next
-        req.user = user; //send id->next function as request
-        next();
-    });
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
+    if (err) return next(errorHandler(403, "Not allowed"));
+    //user is from cookie-id send it to next
+    req.user = user; //send id->next function as request
+    next();
+  });
 };
