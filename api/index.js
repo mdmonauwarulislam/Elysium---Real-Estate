48ce9f50924b696bce741acff8ecf88f03284d5e
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-08-27 23:28:51 +0530

    correction in code

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 //I need apllication string
 mongoose
-  .connect(process.env.MONGODB_URL)
+  .connect(process.env.MONGO)
   .then(() => {
     console.log("Connected to MongoDB!");
   })
@@ -28,10 +28,8 @@ const corsOptions = {
   origin: true,
 };
 app.use(cors(corsOptions));
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(3000, () => {
+  console.log(`Server is running on port 3000!!`);
 });
 
 //api route
@@ -39,6 +37,8 @@ app.listen(PORT, () => {
 //res is from server
 
 app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 //middleware-I use it in auth.controller
 app.use((err, req, res, next) => {
