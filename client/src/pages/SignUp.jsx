e908cc4f77c2572a7c6ffa6c9a8e1f87148e4380
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-09-28 23:05:50 +0530

    Update SignUp.jsx

--- client/src/pages/SignUp.jsx
+++ client/src/pages/SignUp.jsx
@@ -22,7 +22,7 @@ export default function SignUp() {
         setLoading(true);
 
         try {
-            const res = await fetch(`${import.meta.env.VITE_API}/api/auth/signup`, {
+            const res = await fetch(`${import.meta.env.VITE_API}/signup`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
