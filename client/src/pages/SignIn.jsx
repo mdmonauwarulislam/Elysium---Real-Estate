c82843865820e3b4f298414207ea0260061d4698
Md Monauwarul Islam <121303308+mdmonauwarulislam@users.noreply.github.com>
2024-09-28 23:05:31 +0530

    Update SignIn.jsx

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -26,7 +26,7 @@ export default function SignIn() {
 
         try {
             dispatch(signInStart());
-            const res = await fetch(`${import.meta.env.VITE_API}/api/auth/signin`, {
+            const res = await fetch(`${import.meta.env.VITE_API}/signin`, {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
